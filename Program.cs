d28fd63016a1e4e210f4a61c5ca396c0e6acb50c
Zakk <zduncan93@gmail.com>
2022-11-28 22:53:03 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Calculator
+{
+  class Calc
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+                {
+                string oper, yorn;
+                Console.Write("Enter first number: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter second number: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter +, - , *, or / :  ");
+                oper = Console.ReadLine();
+
+                if (oper == "+")
+                {
+                    Console.WriteLine(num1 + num2);
+                }
+                else if (oper == "-")
+                {
+                    Console.WriteLine(num1 - num2);
+                }
+                else if (oper == "*")
+                {
+                    Console.WriteLine(num1 * num2);
+                }
+                else if (oper == "/")
+                {
+                    Console.WriteLine(num1 / num2);
+                }
+                Console.ReadLine();
+                Console.Write("Continue?: ");
+                yorn = Console.ReadLine();
+                if (yorn == "y")
+                {
+                    continue;
+                }
+                else if (yorn == "n")
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+}
